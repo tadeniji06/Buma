6d5d705c3e89d6c0ef450bf399206d9c1a741d93
Olutunmise Adeniji <tadeniji06@gmail.com>
2025-12-11 16:21:52 +0100

    refactor: comment out eslint configuration for build process

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+	// eslint: {
+	// 	ignoreDuringBuilds: true,
+	// },
 	typescript: {
 		ignoreBuildErrors: true,
 	},
 	images: {
+		unoptimized: true,
 		domains: ["cdn.sanity.io"],
 	},
 };
